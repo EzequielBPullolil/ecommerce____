3a04ba74e31c8370bb58956017bce1f2ff7b62e2
EzequielK-source <programacionezequielk@gmail.com>
2022-10-11 20:59:05 -0300

    Add persist test in UploadProduct test -Add sequelize module -Set Postgresql db params in script/test.sh

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,4 +1,8 @@
 export NODE_PATH=.
 export NODE_ENV=testing
-
+export PGUSER=drhades
+export PGHOST=localhost 
+export PGPASSWORD=EzequielEnPsql
+export PGDATABASE=ecommerce_test
+export PGPORT=5432
 mocha test/**/*.test.js --exit
--- test/domain/product/services/upload_product.test.js
+++ test/domain/product/services/upload_product.test.js
@@ -3,18 +3,34 @@ const chai = require('chai')
 chai.use(chaiAsPromised)
 
 const { expect } = chai
-
+//
+const { QueryTypes } = require('sequelize');
+const {sequelize} = require('src/infrastructure/sequelize/index')
 const UploadProduct = require('src/domain/product/services/upload_product')
 describe('UploadProduct test', () => {
+    before(async()=>{
+        await sequelize.query('delete from products')
+    })
     const productSuject = {
         name: 'test'
     }
     it('UploadProduct was fullfilled', async () => {
-        expect(UploadProduct(productSuject))
+        return expect(UploadProduct(productSuject))
             .eventually
             .be
             .fulfilled;
     });
 
-
+    it('Product was persisted', async() => {
+        /**
+         * Makes unmodel query and verify if 
+         * they response with the uploaded product before 
+         */
+        return expect(sequelize.query(`select 1 from products where name='${productSuject.name}'`, { type: QueryTypes.SELECT }))
+            .to
+            .eventually
+            .have
+            .property('length')
+            .equal(1)
+    });
 });
